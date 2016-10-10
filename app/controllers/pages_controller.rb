ea66e626a965a0720631fc3013e234bb5eb3d162
Lee-KangChen <nihaifong@gmail.com>
2016-10-10 20:35:47 +0800

    convention over configuration 3-5-3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 # app/controllers/pages_controller.rb
 class PagesController < ApplicationController
-  def home
-    render 'hello/world'
-  end
-
-  def about
-    render 'pages/about'
-  end
+  # def home
+  #   # render 'hello/world'
+  #   # render 'home'
+  # end
+  #
+  # def about
+  #   # render 'pages/about'
+  #   # render 'about'
+  # end
 end
