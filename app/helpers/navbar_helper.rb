630e93751ed5287ea250f8c0ea30dacb2415e542
Lee-KangChen <nihaifong@gmail.com>
2016-10-11 16:03:25 +0800

    custom Helper 4-3-3

---
+++ app/helpers/navbar_helper.rb
@@ -0,0 +1,7 @@
+# app/helpers/navbar_helper.rb
+module NavbarHelper
+  def nav_li text, path
+    active = request.path == path ? :active : nil
+    content_tag :li, link_to(text, path), class: active
+  end
+end
