2e01e041e3fe4c5268aedf3444aa7f4fd894af83
Lee-KangChen <nihaifong@gmail.com>
2016-10-12 17:13:52 +0800

    custom validates 5-4-4 block style

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 # app/models/post.rb
 class Post < ActiveRecord::Base
   validates :title, :content, presence: true
+  validate do
+    unless title.to_s.start_with? 'X'
+      errors.add(:title, 'must start with "X"')
+    end
+  end
 end
