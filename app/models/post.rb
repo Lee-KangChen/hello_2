253098d796e1af09b8e7565a8c399385bdf5597c
Lee-KangChen <nihaifong@gmail.com>
2016-10-12 17:30:19 +0800

    callback 5-5

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,17 @@
 # app/models/post.rb
 class Post < ActiveRecord::Base
   validates :title, :content, presence: true
-  validate :title_must_start_with_x
+  before_validation :set_content_form_title
 
-  def title_must_start_with_x
-    unless title.to_s.start_with? 'X'
-      errors.add(:title, '必須是「X」開頭')
-    end
+  def set_content_form_title
+    self.content = title if content.blank?
   end
+
+  # validate :title_must_start_with_x
+  #
+  # def title_must_start_with_x
+  #   unless title.to_s.start_with? 'X'
+  #     errors.add(:title, '必須是「X」開頭')
+  #   end
+  # end
 end
