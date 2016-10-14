08138d9d92947049879e6c00b8ac7beeebefed29
Lee-KangChen <nihaifong@gmail.com>
2016-10-14 18:17:21 +0800

    Flash Message 6-5-1

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    flash[:notice] = "You  already create a category"
   end
 
   # GET /categories/new
