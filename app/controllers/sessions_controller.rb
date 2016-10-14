5bd4ce0a7d2f67e5b286f5f6b6cd18327f39bceb
Lee-KangChen <nihaifong@gmail.com>
2016-10-14 22:02:37 +0800

    User Authentication 6-7 helper user_signed_in?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
 # POST /session
   def create
     user = User.find_by(email: params[:email])
+
     if user = user.try(:authenticate, params[:password])
       session[:user_id] = user.id
       message = "Welcome!#{user.name}"
     else
-      message = 'Sign Fail'
+      message = 'Login Fail'
     end
     redirect_to root_path, notice: message
   end
@@ -15,6 +16,6 @@ class SessionsController < ApplicationController
 # DELETE /session
   def destroy
     session.delete(:user_id)
-    redirect_to root_path, notice: 'Sign Out'
+    redirect_to root_path, notice: 'logut'
   end
 end
